029a2f08dddd3a2919c02e284969b1cd65d55a2d
myrisee <risee@Risee-MacBook-Pro.local>
2017-01-21 01:14:35 +0300

    qwe

---
+++ Assets/LeftCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D carpan)
+	{
+		carpan.transform.position += new Vector3 (6075, 0, 0);
+	}
+}
