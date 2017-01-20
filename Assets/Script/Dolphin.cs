63f9946eb6ed59b717e17ac2425492b199ea33e6
Erdal Ural <ahandaerdal@hotmail.com>
2017-01-21 01:12:58 +0300

    Added Dash Move

--- Assets/Script/Dolphin.cs
+++ Assets/Script/Dolphin.cs
@@ -11,16 +11,22 @@ public class Dolphin : MonoBehaviour
     [SerializeField]
     public WhaleMode Mode = WhaleMode.Alpha;
     public float Speed = 400f;
+    public float DashSpeed = 700f;
     public Wave WavePrefab;
     public float MaxChaseDuration = 3f;
     public float MinChaseDuration = 2f;
     public float WaveCooldown = .5f;
+    public float DashTime = 1f;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _movementVector;
     private float _chaseDuration;
     private bool _buttonDown;
-    private float _currentWaveCooldown = 0;
+    private float _currentWaveCooldown;
+    private float _currentDashCooldown;
+    private WaveMode _currentWaveMode = WaveMode.Follow;
+    [SerializeField]
+    private bool _isDashing;
     // Use this for initialization
     void Awake()
     {
@@ -45,15 +51,15 @@ public class Dolphin : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rigidbody.AddForceAtPosition(_movementVector * Speed, transform.position);
+        if (_isDashing)
+            _rigidbody.AddForceAtPosition(_movementVector * DashSpeed, transform.position);
+        else
+            _rigidbody.AddForceAtPosition(_movementVector * Speed, transform.position);
     }
 
     private void RotationUpdate()
     {
-        if (_movementVector.x < 0)
-            transform.localScale = new Vector3(1, -1, 1);
-        else
-            transform.localScale = new Vector3(1, 1, 1);
+        transform.localScale = _movementVector.x < 0 ? new Vector3(1, -1, 1) : new Vector3(1, 1, 1);
 
         var angle = Mathf.Atan2(_movementVector.y, _movementVector.x) * Mathf.Rad2Deg;
         transform.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
@@ -81,7 +87,14 @@ public class Dolphin : MonoBehaviour
     private void ControlActions()
     {
         if (WavePrefab == null) return;
-
+        if (_currentDashCooldown >= 0)
+        {
+            _currentDashCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            _currentDashCooldown = 0;
+        }
         if (_currentWaveCooldown >= 0)
         {
             _currentWaveCooldown -= Time.deltaTime;
@@ -96,24 +109,48 @@ public class Dolphin : MonoBehaviour
         {
             _buttonDown = false;
             _currentWaveCooldown = 0;
+            StartCoroutine(SetAttackMode());
         }
         if (_buttonDown && Math.Abs(_currentWaveCooldown) < float.Epsilon)
         {
-            Instantiate(WavePrefab, transform.position, Quaternion.identity);
+            var wave = Instantiate(WavePrefab, transform.position, Quaternion.identity);
+            wave.Mode = _currentWaveMode;
             _currentWaveCooldown = WaveCooldown;
         }
     }
 
+    private IEnumerator SetAttackMode()
+    {
+        _currentWaveMode = WaveMode.Attack;
+        yield return new WaitForSeconds(WaveCooldown);
+        _currentWaveMode = WaveMode.Follow;
+    }
+
     private void ControlMovement()
     {
-        _movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        if (!_isDashing)
+            _movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
     }
 
     public void SetMovementVector(Vector2 source)
     {
-        if (Mode != WhaleMode.Follower) return;
+        if (Mode != WhaleMode.Follower || _isDashing) return;
         _movementVector = (source - (Vector2)transform.position).normalized;
         _chaseDuration = Random.Range(MinChaseDuration, MaxChaseDuration);
+    }
+
+    public void Dash()
+    {
+        if (_currentDashCooldown > 0) return;
+
+        StartCoroutine(SetDashState());
+    }
+
+    private IEnumerator SetDashState()
+    {
+        _isDashing = true;
+        yield return new WaitForSeconds(DashTime);
+        _isDashing = false;
     }
 }
 public enum WhaleMode { Alpha, Follower }
--- Assets/Script/Wave.cs
+++ Assets/Script/Wave.cs
@@ -8,10 +8,14 @@ public class Wave : MonoBehaviour
     public float Speed = 5f;
     public float FadeSpeed = 10f;
     public float Duration = 1f;
+    public WaveMode Mode;
+
+    private PaletteSwapper _paletteSwapper;
 
     private SpriteRenderer _spriteRenderer;
     void Start()
     {
+        _paletteSwapper = GetComponent<PaletteSwapper>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         Destroy(gameObject, Duration);
     }
@@ -19,13 +23,16 @@ public class Wave : MonoBehaviour
     {
         var dolphinScript = collision.GetComponent<Dolphin>();
         if (dolphinScript == null) return;
-
         dolphinScript.SetMovementVector((Vector2)transform.position);
+        if (Mode == WaveMode.Attack)
+            dolphinScript.Dash();
     }
 
     void Update()
     {
+        _paletteSwapper.CurrentPaletteIndex = (int)Mode;
         transform.localScale = Vector3.Lerp(transform.localScale, MaxRadius * transform.localScale, Time.deltaTime * Speed);
         _spriteRenderer.color = new Color(_spriteRenderer.color.r, _spriteRenderer.color.g, _spriteRenderer.color.b, Mathf.Lerp(_spriteRenderer.color.a, 0, Time.deltaTime * FadeSpeed));
     }
 }
+public enum WaveMode { Follow, Attack }
